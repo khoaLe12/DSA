f0c87b95f8ca5472d1caeb975c3360e6fcf55848
khoaLe12 <lek35067@gmail.com>
2024-12-13 16:35:29 +0700

    Update BST

--- Tree/BinarySearchTree.cs
+++ Tree/BinarySearchTree.cs
@@ -8,4 +8,197 @@ namespace BinaryTree;
 
 internal class BinarySearchTree
 {
+    private Node? _root;
+
+    public BinarySearchTree(Node root)
+    {
+        _root = root;
+    }
+
+    public void Insert(int data)
+    {
+        if(_root is null)
+        {
+            _root = new Node(data);
+            return;
+        }
+
+        Node curr = _root;
+        while (true)
+        {
+            if (data < curr.Data)
+            {
+                if (curr.LeftNode is not null)
+                    curr = curr.LeftNode;
+                else
+                {
+                    curr.LeftNode = new Node(data);
+                    break;
+                }
+            }
+            else if (data > curr.Data)
+            {
+                if (curr.RightNode is not null)
+                    curr = curr.RightNode;
+                else
+                {
+                    curr.RightNode = new Node(data);
+                    break;
+                }
+            }
+            else return;
+        }
+    }
+
+    public Node? Search(int data)
+    {
+        if(_root is null) return null;
+
+        var curr = _root;
+        while(curr != null)
+        {
+            if(curr.Data == data) return curr;
+            if(data < curr.Data) curr = curr.LeftNode;
+            else curr = curr.RightNode;
+        }
+
+        return null;
+    }
+
+    public void Delete(int data)
+    {
+        if (_root is null) return;
+        DeleteNode(_root, data);
+    }
+    private Node? DeleteNode(Node? root, int data)
+    {
+        if (root is null) return null;
+
+        // This is for moving to the finding node
+        if (data < root.Data)
+            root.LeftNode = DeleteNode(root.LeftNode, data);
+        else if (data > root.Data)
+            root.RightNode = DeleteNode(root.RightNode, data);
+        else
+        {
+            // The node is found here
+            // If the node is a leaf or it has only 1 successor,
+            // delete it by replacing it with its successor
+            if(root.LeftNode is null) return root.RightNode;
+            if (root.RightNode is null) return root.LeftNode;
+
+
+            // If the delete node has 2 children, find in-order successor (The smallest node in the right subtree)
+            var successor = GetSuccessor(root.RightNode);
+            root.Data = successor.Data;
+
+            // After replacing the value, need to delete the successor
+            root.RightNode = DeleteNode(root.RightNode, successor.Data);
+        }
+        return root;
+    }
+    private Node GetSuccessor(Node curr)
+    {
+        while(curr != null && curr.LeftNode != null)
+        {
+            curr = curr.LeftNode;
+        }
+        return curr!;
+    }
+
+    public void InorderTraversal()
+    {
+        if(_root is null) return;
+        InorderTraversal(_root);
+    }
+    private void InorderTraversal(Node? node)
+    {
+        if (node is null) return;
+        InorderTraversal(node.LeftNode);
+        Console.Write($"{node.Data} ");
+        InorderTraversal(node.RightNode);
+    }
+
+    public int Minimum()
+    {
+        if (_root is null) return -1;
+        var temp = _root;
+        while (temp.LeftNode is not null)
+            temp = temp.LeftNode;
+        return temp.Data;
+    }
+
+    public int Maximum()
+    {
+        if (_root is null) return -1;
+        var temp = _root;
+        while (temp.RightNode is not null)
+            temp = temp.RightNode;
+        return temp.Data;
+    }
+
+
+    // Find the greatest value that smaller than data
+    // Need to find the value on the 2 sides of a node (2 subtrees)
+    // Start with the right side if data is greater than value of cur node, left side if its smaller
+    // Some properties are critical:
+    // 1. If the current node has value smaller than data, and the right side is null => the result is current node
+    // 2. If the current node has value larger than data, and the left side is null => the result is null (from the current node to below, there is no appropriate node)
+    // 3. If the current node has value smaller than data, and the right side is not null
+    // (there could be a node that is greater than the current node and smaller than data, but it could also none)
+    // => If it exist, return it. But if it is none (null), return the current node
+    // Each recursive will find the result at a specific node (position)
+    public int Floor(int data)
+    {
+        if (_root is null) return -1;
+        var node = Floor(_root, data);
+        if (node is null) return -1;
+        return node.Data;
+    }
+    private Node? Floor(Node node, int data)
+    {
+        if (node.Data == data)
+            return node;
+
+        if (data > node.Data)
+        {
+            if (node.RightNode is null) return node;
+            var result = Floor(node.RightNode, data);
+            if (result is null) return node;
+            return result;
+        }
+        else
+        {
+            if (node.LeftNode is null) return null;
+            return Floor(node.LeftNode, data);
+        }
+    }
+
+
+    // Find the smallest value that greater than data
+    // Same as the above but opposite
+    public int Ceil(int data)
+    {
+        if (_root is null) return -1;
+        var node = Ceil(_root, data);
+        if(node is null) return -1;
+        return node.Data;
+    }
+    private Node? Ceil(Node node, int data)
+    {
+        if (node.Data == data) return node;
+
+        if (node.Data > data) // Go to left to try to find whether if there is a smaller value 
+        {
+            if (node.LeftNode is null) return node;
+            var result = Ceil(node.LeftNode, data);
+            if(result is null) return node;
+            return result;
+        }
+        else // Go to right to find node that greater than data
+        {
+            if(node.RightNode is null) return null;
+            return Ceil(node.RightNode, data);
+        }
+    }
 }
--- Tree/Program.cs
+++ Tree/Program.cs
@@ -1,81 +1,86 @@
 ﻿using BinaryTreee = BinaryTree.BinaryTree;
 using BinaryTree;
 
-var root = new Node(10);
-// Lv1
-root.LeftNode = new Node(1);
-root.RightNode = new Node(3);
-// Lv2
-root.LeftNode.LeftNode = new Node(5);
-root.LeftNode.RightNode = new Node(7);
-root.RightNode.LeftNode = new Node(9);
-// Lv3
-root.LeftNode.LeftNode.RightNode = new Node(11);
-// Lv4
-root.LeftNode.LeftNode.RightNode.LeftNode = new Node(12);
-root.LeftNode.LeftNode.RightNode.RightNode = new Node(6);
 
-// Tree
-// Lv0                        10(x1)
-// Lv1             1                      3
-// Lv2       5(x2)       7           9         x1
-// Lv3    x2    11(x3)
-// Lv4       12     6
-// Lv5    x3
+//DemoBinaryTree();
 
-BinaryTreee tree = new BinaryTreee(root);
+DemoBinarySearchTree();
 
-Console.Write("Pre Order DFS: ");
-tree.PreorderDFS();
 
-Console.Write("\n\nIn Order DFS: ");
-tree.InorderDFS();
 
-Console.Write("\n\nPost Order DFS: ");
-tree.PostorderDFS();
 
-Console.Write("\n\nBFS: ");
-tree.BFS();
-
-Console.WriteLine($"\n\nSearch DFS 12: {tree.SearchDFS(12) is not null}");
-Console.WriteLine($"Search BFS 12: {tree.SearchBFS(12) is not null}");
-
-Console.WriteLine($"Search DFS 9: {tree.SearchDFS(9) is not null}");
-Console.WriteLine($"Search BFS 9: {tree.SearchBFS(9) is not null}");
-
-Console.WriteLine($"Search DFS 7: {tree.SearchDFS(7) is not null}");
-Console.WriteLine($"Search BFS 7: {tree.SearchBFS(7) is not null}");
-
-Console.WriteLine($"Search DFS 4: {tree.SearchDFS(4) is not null}");
-Console.WriteLine($"Search BFS 4: {tree.SearchBFS(4) is not null}");
-
-//Demo1();
-
-Console.Write($"\n\nFind height using BFS: {tree.GetHeightBFS()}");
-Console.Write($"\n\nFind height using DFS: {tree.GetHeightDFS()}");
-
-var searchNode12 = tree.SearchBFS(12);
-if(searchNode12 is null)
-    Console.Write("\n\nNode with data 12 not found");
-else
+void DemoBinaryTree()
 {
-    var ancestor = tree.FindAncestor(searchNode12);
-    Console.Write($"\n\nAncestor of node 12 is: {ancestor?.Data}");
+    var root = new Node(10);
+    // Lv1
+    root.LeftNode = new Node(1);
+    root.RightNode = new Node(3);
+    // Lv2
+    root.LeftNode.LeftNode = new Node(5);
+    root.LeftNode.RightNode = new Node(7);
+    root.RightNode.LeftNode = new Node(9);
+    // Lv3
+    root.LeftNode.LeftNode.RightNode = new Node(11);
+    // Lv4
+    root.LeftNode.LeftNode.RightNode.LeftNode = new Node(12);
+    root.LeftNode.LeftNode.RightNode.RightNode = new Node(6);
 
-    var descendants = tree.FindDescendants(searchNode12);
-    Console.Write($"\n\nDescendants of node 12 is: ");
-    foreach(var node in descendants)
+    // Tree
+    // Lv0                        10(x1)
+    // Lv1             1                      3
+    // Lv2       5(x2)       7           9         x1
+    // Lv3    x2    11(x3)
+    // Lv4       12     6
+    // Lv5    x3
+
+    BinaryTreee tree = new BinaryTreee(root);
+
+    Console.Write("Pre Order DFS: ");
+    tree.PreorderDFS();
+
+    Console.Write("\n\nIn Order DFS: ");
+    tree.InorderDFS();
+
+    Console.Write("\n\nPost Order DFS: ");
+    tree.PostorderDFS();
+
+    Console.Write("\n\nBFS: ");
+    tree.BFS();
+
+    Console.WriteLine($"\n\nSearch DFS 12: {tree.SearchDFS(12) is not null}");
+    Console.WriteLine($"Search BFS 12: {tree.SearchBFS(12) is not null}");
+
+    Console.WriteLine($"Search DFS 9: {tree.SearchDFS(9) is not null}");
+    Console.WriteLine($"Search BFS 9: {tree.SearchBFS(9) is not null}");
+
+    Console.WriteLine($"Search DFS 7: {tree.SearchDFS(7) is not null}");
+    Console.WriteLine($"Search BFS 7: {tree.SearchBFS(7) is not null}");
+
+    Console.WriteLine($"Search DFS 4: {tree.SearchDFS(4) is not null}");
+    Console.WriteLine($"Search BFS 4: {tree.SearchBFS(4) is not null}");
+
+    //Demo1(tree);
+
+    Console.Write($"\n\nFind height using BFS: {tree.GetHeightBFS()}");
+    Console.Write($"\n\nFind height using DFS: {tree.GetHeightDFS()}");
+
+    var searchNode12 = tree.SearchBFS(12);
+    if (searchNode12 is null)
+        Console.Write("\n\nNode with data 12 not found");
+    else
     {
-        Console.Write($"{node.Data} ");
+        var ancestor = tree.FindAncestor(searchNode12);
+        Console.Write($"\n\nAncestor of node 12 is: {ancestor?.Data}");
+
+        var descendants = tree.FindDescendants(searchNode12);
+        Console.Write($"\n\nDescendants of node 12 is: ");
+        foreach (var node in descendants)
+        {
+            Console.Write($"{node.Data} ");
+        }
     }
 }
-
-
-
-
-
-
-void Demo1()
+void Demo1(BinaryTreee tree)
 {
     var searchNode10 = tree.SearchDFS(10);
     if (searchNode10 is null)
@@ -116,4 +121,72 @@ void Demo1()
     tree.DeleteNode(10);
     Console.Write("\n\nAfter deleting node with value 10: ");
     tree.BFS();
+}
+
+
+void DemoBinarySearchTree()
+{
+    var root = new Node(10);
+
+    BinarySearchTree tree = new(root);
+    tree.Insert(3);
+    tree.Insert(5);
+    tree.Insert(1);
+    tree.Insert(16);
+    tree.Insert(20);
+    tree.Insert(14);
+    tree.Insert(25);
+    tree.Insert(9);
+    // Tree
+    // Lv0                10
+    // Lv1        3                  16
+    // Lv2      1   5             14    20
+    // Lv2            9                   25
+
+
+    Console.Write("Traverse the tree: ");
+    tree.InorderTraversal();
+
+
+    var node10 = tree.Search(10);
+    var node14 = tree.Search(14);
+    var node9 = tree.Search(9);
+    var node5 = tree.Search(5);
+    Console.WriteLine($"\n\nNode 10: {node10?.Data}");
+    Console.WriteLine($"Node 14: {node14?.Data}");
+    Console.WriteLine($"Node 9: {node9?.Data}");
+    Console.Write($"Node 5: {node5?.Data}");
+
+    //DemoDeleteInBST(tree);
+
+
+    Console.WriteLine($"\n\nThe minimum value of tree is: {tree.Minimum()}");
+    Console.Write($"The maximum value of tree is: {tree.Maximum()}");
+
+
+    Console.WriteLine($"\n\nThe floor of 15 is: {tree.Floor(15)}"); // Expected 14
+    Console.WriteLine($"The floor of 14 is: {tree.Floor(14)}"); //  Expected 14
+    Console.Write($"The floor of 13 is: {tree.Floor(13)}"); // Expected 10
+
+
+    Console.WriteLine($"\n\nThe ceil of 13 is: {tree.Ceil(13)}"); // Expected 14
+    Console.WriteLine($"The ceil of 6 is: {tree.Ceil(6)}"); //  Expected 9
+    Console.WriteLine($"The ceil of 2 is: {tree.Ceil(2)}"); // Expected 3
+    Console.Write($"The ceil of 3 is: {tree.Ceil(3)}"); // Expected 3
+
+}
+
+void DemoDeleteInBST(BinarySearchTree tree)
+{
+    tree.Delete(10); // Expect 10 is replaced by 14
+    Console.Write("\n\nTraverse the tree: ");
+    tree.InorderTraversal();
+
+    tree.Delete(20); // Expect 20 is replaced by 25
+    Console.Write("\n\nTraverse the tree: ");
+    tree.InorderTraversal();
+
+    tree.Delete(25); // Expect 25 is deleted
+    Console.Write("\n\nTraverse the tree: ");
+    tree.InorderTraversal();
 }
